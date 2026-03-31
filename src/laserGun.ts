88baa0f7a39de8999d9a3fd45750ee82c8a45988
Herdis Heida <herdisj24@ru.is>
2026-03-31 12:27:13 +0000

    laser gun ready :DD

---
+++ src/laserGun.ts
@@ -0,0 +1,15 @@
+import { addAnimationClass, addShot } from "./common";
+import { subscribe } from "./observer";
+
+export function laserGun() {
+  subscribe("machine_gun_done", () => {
+    addAnimationClass("laser-gun");
+
+    // 10 lasers
+    for (let i = 0; i < 10; i++) {
+      addShot("laser", "laser-gun", 10, "left", i * 1000 + 500);
+    }
+  });
+
+  // no publish -> it's the last one in the chain
+}

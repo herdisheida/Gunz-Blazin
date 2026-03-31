33aee8be4420a5a453589d17c55fd1f550bd0f4f
Herdis Heida <herdisj24@ru.is>
2026-03-31 12:14:00 +0000

    uzi working

---
+++ src/uzi.ts
@@ -0,0 +1,19 @@
+import {
+  addAnimationClass,
+  addShot,
+  publishTopicAfterXSeconds,
+} from "./common";
+import { subscribe } from "./observer";
+
+export function uzi() {
+  subscribe("pistol_done", () => {
+    addAnimationClass("uzi");
+
+    // 10 bullets
+    for (let i = 0; i < 10; i++) {
+      addShot("bullet", "uzi", 70, "left", i * 200);
+    }
+
+    publishTopicAfterXSeconds("uzi_done", 1000);
+  });
+}

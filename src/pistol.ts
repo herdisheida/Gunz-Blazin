581066653bace35c922ccdc386868c63a000160a
Herdis Heida <herdisj24@ru.is>
2026-03-31 12:07:43 +0000

    pistol animation working

---
+++ src/pistol.ts
@@ -0,0 +1,15 @@
+import {
+  addAnimationClass,
+  addShot,
+  publishTopicAfterXSeconds,
+} from "./common";
+
+export function pistol() {
+  addAnimationClass("pistol");
+
+  // 2 bullets
+  addShot("bullet", "pistol", 50, "right", 0);
+  addShot("bullet", "pistol", 50, "right", 650);
+
+  publishTopicAfterXSeconds("pistol_done", 1000);
+}

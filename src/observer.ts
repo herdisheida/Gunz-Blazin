bb12f1a4414f9b061386a0bfd83756b27048854d
Herdis Heida <herdisj24@ru.is>
2026-03-31 10:55:29 +0000

    template

--- src/observer.ts
+++ src/observer.ts
@@ -1,3 +1,6 @@
 /* Here you should implement the observer pattern */
 
 export function publish(_topic: string): void {}
+
+
+export function subscribe(_topic: string, _callback: () => void): void {}
